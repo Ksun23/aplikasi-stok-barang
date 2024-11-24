d014ff80a402695cc891a56e8b4781e5e36bbf18
Ksun <muktieka16@gmail.com>
2024-11-24 18:12:26 +0700

    speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const geistSans = localFont({
@@ -31,7 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
